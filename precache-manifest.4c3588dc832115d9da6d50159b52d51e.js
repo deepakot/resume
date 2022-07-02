c3b5fe8799f6287a3c540a33e6bddcf7a7ca143d
Deepak Sharma <deepak@digitised.energy>
2022-07-02 13:35:39 +0530

    Updates

---
+++ precache-manifest.4c3588dc832115d9da6d50159b52d51e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0183499d78b64eed88fa",
+    "url": "/resume/static/css/main.ae7de559.chunk.css"
+  },
+  {
+    "revision": "0183499d78b64eed88fa",
+    "url": "/resume/static/js/main.d96d6853.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  },
+  {
+    "revision": "a7cf0d40aef3728eb490",
+    "url": "/resume/static/js/2.2bd585a4.chunk.js"
+  },
+  {
+    "revision": "d0b71da48262c243249c47fd6ef9f7e5",
+    "url": "/resume/static/media/deepak.d0b71da4.png"
+  },
+  {
+    "revision": "19b2a9086b1e9c2eeafebec09c1460ce",
+    "url": "/resume/index.html"
+  }
+];
